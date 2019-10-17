b8ac8d6912b044e63893b95be0dd8a84ba691039
Nick <55153307+Nickhockman@users.noreply.github.com>
2019-10-17 00:40:07 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var img;
+function preload(){
+img = loadImage("project3/the.png");
+}
+
+function setup() {
+  createCanvas(800,400);
+
+}
+
+function draw() {
+  background(109);
+ellipse(50,50,90,20);
+  rect(50,70,20,100);
+  fill(0,0,255);
+  rect(360,320,110,20);
+  
+  fill(255,0,0);
+  rect(290,70,20,100);
+  rect(360,120,20,50);
+  rect(50,240,20,100);
+  rect(160,240,20,100);
+  rect(300,240,20,100);
+  rect(360,120,20,50);
+    image(img,400,50)
+}
